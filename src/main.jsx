a4f92a4264ad669ef75b6301fb4e86c8648107f9
Dali <satinktten@gmail.com>
2025-06-07 16:44:46 +0300

    auth bug fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import { StrictMode } from "react";
+import { StrictMode, useEffect } from "react";
 import "./assets/css/index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
@@ -9,33 +9,46 @@ import { Provider } from "react-redux";
 import "./config/i18n";
 import { login } from "./services/authService";
 
-//authorization
-const telegram = window.Telegram?.WebApp;
+export default function Root() {
+   //authorization
+   const telegram = window.Telegram?.WebApp;
 
-let initData = telegram?.initData || null;
+   let initData = telegram?.initData || null;
 
-//fallback for testing outside Telegram
-if (!initData) {
-   initData =
-      "id=124456789&first_name=test&username=test&auth_date=1700000000&hash=06a5ad71c2d2322be7ecb8f03b317c9c27140768c7429ebce05c7a9dcde4e6c5";
-}
+   //fallback for testing outside Telegram
+   if (!initData) {
+      initData =
+         "id=124456789&first_name=test&username=test&auth_date=1700000000&hash=06a5ad71c2d2322be7ecb8f03b317c9c27140768c7429ebce05c7a9dcde4e6c5";
+   }
 
-if (initData) {
-   login(initData);
-}
+   if (initData) {
+      login(initData);
+   }
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-   <StrictMode>
+   useEffect(() => {
+      // сразу пытаемся залогинить пользователя
+      login().catch((err) =>
+         console.error("Telegram login failed:", err.message)
+      );
+   }, []);
+
+   return (
       <BrowserRouter>
          <Provider store={store}>
             <div
                className="d-flex flex-column flex-grow-1 h-100 bg-container"
-               style={{ padding: "9vh 0 7vh 0" }}
+               style={{ padding: "7vh 0" }}
             >
                <img src={textureBg} alt="texture" className="texture" />
                <App />
             </div>
          </Provider>
       </BrowserRouter>
+   );
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+   <StrictMode>
+      <Root />
    </StrictMode>
 );
--- src/services/authService.js
+++ src/services/authService.js
@@ -2,13 +2,29 @@ import axios from "axios";
 
 const API_URL = import.meta.env.VITE_API_URL;
 
-export const login = async (credentials) => {
+export async function login() {
+   const urlBase = `${API_URL}/auth/telegram`;
+
    try {
-      const response = await axios.post(`${API_URL}/auth/telegram`, {
-         initData: credentials,
-      });
-      localStorage.setItem("token", response.data.access_token);
+      if (window.location.search.includes("hash=")) {
+         // Если данные пришли через query-параметры
+         const queryParams = new URLSearchParams(window.location.search);
+         const queryString = queryParams.toString();
+
+         const { data } = await axios.post(`${urlBase}?${queryString}`);
+         localStorage.setItem("token", data.access_token);
+      } else if (window.Telegram?.WebApp?.initData) {
+         // Если данные есть в Telegram.WebApp.initData
+         const { data } = await axios.post(urlBase, {
+            initData: window.Telegram.WebApp.initData,
+         });
+         localStorage.setItem("token", data.access_token);
+      } else {
+         console.warn("Telegram auth data not found");
+      }
    } catch (error) {
-      console.error(error);
+      const errMsg =
+         error.response?.data?.detail || error.message || "Login failed";
+      throw new Error(errMsg);
    }
-};
+}
