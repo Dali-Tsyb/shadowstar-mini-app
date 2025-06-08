754e9716fc52ff887a99e8807abe0ae7cbc6f1ed
Dali <satinktten@gmail.com>
2025-06-09 01:50:46 +0300

    auth bug fix

--- src/services/authService.js
+++ src/services/authService.js
@@ -45,6 +45,7 @@ export const login = async (initDataString) => {
       const { token } = response.data;
       localStorage.setItem("token", token);
       axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+      axios.defaults.headers.common["X-Telegram-Init-Data"] = initDataString;
    } catch (error) {
       console.error("Auth error:", error);
    }
